79a149603ab912dc6ea21d29fa31585c05c001b1
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-01-09 12:34:01 +0530

    Almost done with nested Object Iterator

--- Day 3/part3.js
+++ Day 3/part3.js
@@ -1,26 +1,30 @@
 // Iterator Assignment
 
+const nestedObjectIterate = (obj) => {
+    let nestedObjectChecker =
+        obj instanceof Array === false &&
+        typeof obj === "object";
+
+    Object.keys(obj).map((val) => {
+        if ( nestedObjectChecker === true) {
+            nestedObjectIterate(obj[val])
+        }
+        console.log(obj[val]);
+    });
+};
+
 const iteratorFunc = (value) => {
     let indicator = 0; // checks if value is Array or Object
-    // console.log(indicator);
-
-    const nestedObjectIterate = (obj) => {};
 
     return {
         next: () => {
             if (value instanceof Array) {
-                console.log(value[indicator]);
+                console.log(value[indicator++]);
             } else {
-                let currObject = value[Object.keys(value)[indicator]];
-                let nestedObjectChecker =
-                    currObject instanceof Array === false &&
-                    typeof currObject === "object";
-
-                nestedObjectChecker === true
-                    ? iteratorFunc(currObject).next()
-                    : console.log(currObject);
+                let currObject = value[Object.keys(value)[indicator++]];
+                // console.log(currObject);
+                nestedObjectIterate(currObject);
             }
-            indicator++;
         },
     };
 };
@@ -28,8 +32,11 @@ const iteratorFunc = (value) => {
 const iterator = iteratorFunc({
     arr: [1, 2, 3],
     name: "Joyal",
-    nested: { nest: true, nextneseted: {age: 21} },
-    ageed: "yes"
+    nested: { nest: true, nextneseted: { age: 21 } },
+    ageed: "yes",
 });
 
+iterator.next();
+iterator.next();
+iterator.next();
 iterator.next();
